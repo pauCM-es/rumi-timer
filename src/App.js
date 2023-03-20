149a6725b38c18e346b6fadb9a36e304ffc08346
pauCM-es <costamingarro@gmail.com>
2023-03-20 15:15:58 +0100

    feat add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import './App.css';
+import Landing from "./pages/landing/Landing";
+import GameSettings from "./pages/game-settings/GameSettings";
+import Timer from "./pages/timer/Timer";
+import ScoreBoard from "./pages/score-board/ScoreBoard";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Landing /> } />
+        <Route path="/settings" element={ <GameSettings /> } />
+        <Route path="/timer" element={ <Timer /> } />
+        <Route path="/score-board" element={ <ScoreBoard /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
