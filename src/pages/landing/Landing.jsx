435cf115c54de5dec0c526b205d6185825bd0f17
pauCM-es <costamingarro@gmail.com>
2023-03-20 16:03:31 +0100

    feat add landing page

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export const Landing = () => {
   return (
-    <div>Landing</div>
+    <section
+    className='bg-gradient-to-tr from-fuchsia-500 to-cyan-500 h-screen w-screen flex flex-col items-center justify-center gap-20'
+    >
+      <div className="text-white text-5xl text-center">
+        LET'S PLAY RUMI!!!
+      </div>
+      <Link 
+      to={"/settings"}
+      className="bg-slate-700 text-white text-3xl border-2 border-white rounded-lg px-3 py-1 "
+      >
+        START</Link>
+    </section>
   )
 }
 
